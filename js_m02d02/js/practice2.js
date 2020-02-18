faa71add1affedf1c4917a19155a6455db3c7049
Yarotskiy Sergey <abrawa@gmail.com>
2020-02-18 17:36:45 +0200

    practice functions task4 done

--- js_m02d02/js/practice2.js
+++ js_m02d02/js/practice2.js
@@ -70,3 +70,21 @@
 // console.log(findLongestWord("May the force be with you")); // 'force'
 
 // ------------------------TASK 04---------------
+
+const formatString = function(string) {
+  if (string.length <= 40) {
+    return string;
+  } else {
+    const newString = `${string.slice(0, 40)}...`;
+    return newString;
+  }
+};
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+console.log(formatString("Curabitur ligula sapien."));
+console.log(
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
+);
